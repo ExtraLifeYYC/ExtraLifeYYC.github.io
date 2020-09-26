f1a5aba169b21e2b5998b68c52dac27ab32031c8
Todd <todd@willsie.net>
2020-09-26 00:45:18 -0600

    added team donation progress bar.

---
+++ assets/js/team-goal.js
@@ -0,0 +1,28 @@
+(function( $, ELT ){
+	/**********
+	 * Main Functionality
+	 **********/
+
+	/* Initial setup of the layout and theme based on user settings */
+	function start() {
+		update();
+	}
+
+	/* Main loop */
+	function update() {
+		ELT.api.team(ELT.settings.teamId, onSuccess);
+	}
+
+	function onSuccess(result) {
+		const $raised = $('#elRaised');
+		const $goal = $('#elGoal');
+		const $header = $('#extralifegoal');
+		$calcratio = ((result.sumDonations / result.fundraisingGoal) * 100).toFixed(0);
+		$raised.html(ELT.toCurrency(result.sumDonations));
+		$goal.html(ELT.toCurrency(result.fundraisingGoal));
+		
+		$header.attr("data-value", $calcratio)
+	}
+
+	start();
+})(window.jQuery, window.ELT);
