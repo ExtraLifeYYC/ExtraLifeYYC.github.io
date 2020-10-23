d165a79af18fae8247095206d69d5e2af63a12ac
Todd <todd@willsie.net>
2020-10-23 02:05:18 -0600

    Automatic Currency Conversion on Build & Canadian Guild Overlays

--- assets/js/team-goal-settings.js
+++ assets/js/team-goal-settings.js
@@ -1,6 +1,12 @@
+teamCode = getUrlParameter("teamid");
+
+if(teamCode == ""){
+	teamCode = 50312
+}
+
 ELT.settings = {
 	// Single team to get information for.
-	teamId: "50312",
+	teamId: teamCode,
 	
 	// how often should data be refreshed
 	refreshTimeMS: 10000,
