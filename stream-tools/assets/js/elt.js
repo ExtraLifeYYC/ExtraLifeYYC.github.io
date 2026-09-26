1e617dc00455638f7566b2283b4894652e8735fc
Willsie-Digital <Willsie-Digital@users.noreply.github.com>
2026-09-26 15:57:55 +0000

    jekyll build from Action 587ea5d205a90719003a2c4a65fb75ec88cd78b5

---
+++ stream-tools/assets/js/elt.js
@@ -0,0 +1,42 @@
+const currencyOptions = {
+	formatWithSymbol: true,
+	precision: 2,
+	separator: ',',
+};
+const baseURL = "https://www.extra-life.org/api/";
+
+function createApiActionFor(fuseaction){
+	return function(data, callback) {
+		let timestamp = {
+			timestamp: new Date().getTime()
+		};
+		
+		$.ajax({
+			type: 'GET',
+			url: baseURL + fuseaction.replace("{}", data),
+			timestamp,
+			success: callback,
+			error: function() {
+				callback({});
+			}
+		});
+	};
+}
+
+window.ELT = {
+	isEmpty: function(value) {
+		return (value == null || value === "");
+	},
+
+	toCurrency: function( value ) {
+		return currency(value, currencyOptions).format();
+	},
+
+	api: {
+		participant: createApiActionFor('participants/{}'),
+		participantDonations: createApiActionFor('participants/{}/donations'),
+		team: createApiActionFor('teams/{}'),
+		teamParticipants: createApiActionFor('teams/{}/participants'),
+		teamDonations: createApiActionFor('teams/{}/donations'),
+	},
+};
